acdd5bd7bb2bf58f92e0c4c68a738f85c17957e1
forthenew <forthenew@gmail.com>
2021-01-01 15:40:47 +0900

    세그먼트와 다른 아이템의 위치 조정

--- Map/ViewController.swift
+++ Map/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var lblTitle: UILabel!
+    @IBOutlet var lblAddress: UILabel!
+    
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        lblTitle.text = ""
+        lblAddress.text = ""
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+//        let coor = locationManager.location?.coordinate
+        mapView.showsUserLocation = true
+        
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let pLocation = locations.last
+        _ = goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
+//        CLGeocoder().rev
+    }
+    
+    func goLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double) -> CLLocationCoordinate2D {
+        let pLocation = CLLocationCoordinate2D(latitude: latitudeValue, longitude: longitudeValue)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+        mapView.setRegion(pRegion, animated: true)
+        
+        return pLocation
+    }
+    
+    @IBAction func segment(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            self.lblTitle.text = ""
+            self.lblAddress.text = ""
+            locationManager.startUpdatingLocation()
+        } else if sender.selectedSegmentIndex == 1 {
+            self.lblTitle.text = "우리집"
+            self.lblAddress.text = "경기도 안양시 동안구 부림로 80"
+        } else {
+            self.lblTitle.text = "(주)에스티에이엠"
+            self.lblAddress.text = "서울특별시 구로구 구로동"
+        }
+    }
 }
 
